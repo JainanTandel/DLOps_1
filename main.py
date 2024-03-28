479d94af7a5ff66478414fc30b2f0a018ff23933
Jainan-Tandel <jainan11.valsad@gmail.com>
2024-03-29 00:17:39 +0530

    Feature 2 relu,tanh,leakyrelu on randomvals

--- main.py
+++ main.py
@@ -32,3 +32,8 @@ plt.ylabel('Output')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+for x in random_values: print(relu(x))
+for x in random_values: print(tanh(x))
+for x in random_values: print(leaky_relu(x))
