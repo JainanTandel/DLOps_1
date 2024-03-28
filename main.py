f6a35ff9d2c90bc1c0019bf5adf9231d319a1f5f
Jainan-Tandel <jainan11.valsad@gmail.com>
2024-03-29 00:05:14 +0530

    main Activity 0 code

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def tanh(x):
+    return np.tanh(x)
+
+def leaky_relu(x, alpha=0.02):
+    return np.where(x >= 0, x, alpha * x)
+
+x = np.linspace(-5, 5, 100)
+
+sigmoid_y = sigmoid(x)
+relu_y = relu(x)
+tanh_y = tanh(x)
+leaky_relu_y = leaky_relu(x)
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, sigmoid_y, label='Sigmoid')
+plt.plot(x, relu_y, label='ReLU')
+plt.plot(x, tanh_y, label='Tanh')
+plt.plot(x, leaky_relu_y, label='Leaky ReLU')
+plt.title('Activation Functions')
+plt.xlabel('Input')
+plt.ylabel('Output')
+plt.legend()
+plt.grid(True)
+plt.show()
